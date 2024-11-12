669edc655b48694c44815e3624967dfcd8124a11
Senjumarru <alinursarsekov315@gmail.com>
2024-11-12 19:40:13 +0500

    Добавьте файлы проекта.

---
+++ Composite/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+
+abstract class OrganizationComponent
+{
+    public abstract string GetName();
+    public abstract double GetSalary();
+    public abstract int GetEmployeeCount();
+    public virtual void Add(OrganizationComponent component) { }
+    public abstract void DisplayStructure(int indent = 0);
+}
+
+class Employee : OrganizationComponent
+{
+    private string name;
+    private string position;
+    private double salary;
+
+    public Employee(string name, string position, double salary)
+    {
+        this.name = name;
+        this.position = position;
+        this.salary = salary;
+    }
+
+    public override string GetName() => name;
+    public override double GetSalary() => salary;
+    public override int GetEmployeeCount() => 1;
+
+    public override void DisplayStructure(int indent = 0)
+    {
+        Console.WriteLine(new String(' ', indent) + $"{position}: {name} (Зарплата: {salary})");
+    }
+}
+
+class Department : OrganizationComponent
+{
+    private string name;
+    private List<OrganizationComponent> components = new List<OrganizationComponent>();
+
+    public Department(string name)
+    {
+        this.name = name;
+    }
+
+    public override string GetName() => name;
+
+    public override double GetSalary()
+    {
+        double totalSalary = 0;
+        foreach (var component in components)
+        {
+            totalSalary += component.GetSalary();
+        }
+        return totalSalary;
+    }
+
+    public override int GetEmployeeCount()
+    {
+        int count = 0;
+        foreach (var component in components)
+        {
+            count += component.GetEmployeeCount();
+        }
+        return count;
+    }
+
+    public override void Add(OrganizationComponent component)
+    {
+        components.Add(component);
+    }
+
+    public override void DisplayStructure(int indent = 0)
+    {
+        Console.WriteLine(new String(' ', indent) + "Отдел: " + name);
+        foreach (var component in components)
+        {
+            component.DisplayStructure(indent + 2);
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var company = new Department("Корпорация");
+
+        var itDept = new Department("ИТ-отдел");
+        itDept.Add(new Employee("Alinur", "Программист", 3000));
+        itDept.Add(new Employee("Bauka", "Тестировщик", 2500));
+
+        var hrDept = new Department("HR-отдел");
+        hrDept.Add(new Employee("Khngld", "Рекрутер", 2000));
+
+        var subDept = new Department("Подотдел ИТ");
+        subDept.Add(new Employee("Aisyn", "Младший разработчик", 1500));
+        itDept.Add(subDept);
+
+        company.Add(itDept);
+        company.Add(hrDept);
+
+        Console.WriteLine("Структура компании:");
+        company.DisplayStructure();
+
+        Console.WriteLine($"\nОбщий бюджет компании: {company.GetSalary()}");
+        Console.WriteLine($"Общее количество сотрудников: {company.GetEmployeeCount()}");
+    }
+}
---
+++ Modul10/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+class RoomBookingSystem
+{
+    public string BookRoom() => "Номер забронирован";
+    public string CancelRoom() => "Бронирование номера отменено";
+    public string CheckAvailability() => "Проверка доступности номеров";
+}
+
+class RestaurantSystem
+{
+    public string ReserveTable() => "Стол забронирован";
+    public string OrderFood() => "Еда заказана";
+    public string CallTaxi() => "Такси вызвано";
+}
+
+class EventManagementSystem
+{
+    public string BookConferenceRoom() => "Конференц-зал забронирован";
+    public string OrderEquipment() => "Оборудование заказано";
+}
+
+class CleaningService
+{
+    public string ScheduleCleaning() => "Уборка запланирована";
+    public string PerformCleaning() => "Уборка выполнена";
+}
+
+class HotelFacade
+{
+    private RoomBookingSystem roomBooking = new RoomBookingSystem();
+    private RestaurantSystem restaurant = new RestaurantSystem();
+    private EventManagementSystem eventManagement = new EventManagementSystem();
+    private CleaningService cleaningService = new CleaningService();
+
+    public string BookRoomWithService()
+    {
+        return $"{roomBooking.BookRoom()}, {restaurant.OrderFood()}, {cleaningService.ScheduleCleaning()}";
+    }
+
+    public string OrganizeEventWithRooms()
+    {
+        return $"{eventManagement.BookConferenceRoom()}, {eventManagement.OrderEquipment()}, {roomBooking.BookRoom()}";
+    }
+
+    public string ReserveRestaurantTableWithTaxi()
+    {
+        return $"{restaurant.ReserveTable()}, {restaurant.CallTaxi()}";
+    }
+
+    public string CancelRoomBooking()
+    {
+        return roomBooking.CancelRoom();
+    }
+
+    public string RequestCleaning()
+    {
+        return cleaningService.PerformCleaning();
+    }
+}
+
+// Клиентский код
+class Program
+{
+    static void Main()
+    {
+        var hotelFacade = new HotelFacade();
+        Console.WriteLine(hotelFacade.BookRoomWithService());
+        Console.WriteLine(hotelFacade.OrganizeEventWithRooms());
+        Console.WriteLine(hotelFacade.ReserveRestaurantTableWithTaxi());
+        Console.WriteLine(hotelFacade.CancelRoomBooking());
+        Console.WriteLine(hotelFacade.RequestCleaning());
+    }
+}
